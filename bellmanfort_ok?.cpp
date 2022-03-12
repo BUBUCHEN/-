096117fdfc74f9a08bdb05e801056e4b59b04058
BUBUCHEN <75531008+BUBUCHEN@users.noreply.github.com>
2022-03-12 12:47:53 +0800

    Update bellmanfort_ok?.cpp

--- bellmanfort_ok?.cpp
+++ bellmanfort_ok?.cpp
@@ -1,111 +1,99 @@
-#include<iostream>
-#include<fstream>
-#include<vector>
-#include<string>
 #include<algorithm>
-#define on_fire_para 1
-#define out_speed 2
-#define inf 2147483647
+#include <iostream>
+#include <string>
+#include<vector>
+#include<fstream>
+#define inf 9999999
 using namespace std;
-struct pair_s{
-	int nxt;
-	int value;
+struct pair_s{//自製pair
+		int nxt;//下一個節點
+		int value;//權重
 };
-struct node_s{
+struct node_struct{
+	int now_value;
+	int on_fire_t;
 	int x;
 	int y;
 	int from;
-	int now_min;
-	int on_fire_t;
+	int was_father;
 	vector<pair_s> child;
 	vector<pair_s> father;
 };
-	int n;
-	int s,e;
-	node_s node[200];	
-void bellmanford(int s)
-{
-  int start = s;
-  node[start].now_min = 0;
-  bool change =true;
-  for (int i=1; i<=n; i++)
-  {
-  	node[i].now_min=inf;
-  	node[i].from=-1;
-  }
-  for(int i=0;i<node[start].child.size();i++){
-  	node[node[start].child[i].nxt].from=start;
-  }
-  for(int i=0; i<n-2 && change;  i++)
-  {
-    change = false;
-    for (int j=1; j<=n; j++)
-    {
-      if(j!= start)
-      {
-      	for(int g=0;g<node[j].child.size();g++){
-      		if(node[node[j].child[g].nxt].now_min>node[j].now_min+node[j].child[g].value && node[j].now_min+node[j].child[g].value<node[node[j].child[g].nxt].on_fire_t)
-            {
-              node[node[j].child[g].nxt].now_min = node[j].now_min+node[j].child[g].value;
-              node[node[j].child[g].nxt].from=j;
-              change = true;
-            }		
-		} 
-      }
-    }
-  }
-}
-void bfs_fire_spread(int tot,int now){
-	if(node[now].on_fire_t>=tot*2){
-		node[now].on_fire_t=tot*2;
-		for(int i=0;i<node[now].child.size();i++){
-			bfs_fire_spread(tot+node[now].child[i].value,node[now].child[i].nxt);
-		}
-		for(int i=0;i<node[now].father.size();i++){
-			bfs_fire_spread(tot+node[now].father[i].value,node[now].father[i].nxt);
-		}
-	}
-	return ;
-} 
-void input(){
-	pair_s tmp;
-	int count,nxt,val,o,of;
-	ifstream ifs;
-	ifs.open("input.txt");
-	ifs>>n;
-	for(int i=1;i<=n;i++){
-  		node[i].on_fire_t=inf;
-  	}
-	for(int i=1;i<=n;i++){
-		ifs>>count;
-		for(int v=0;v<count;v++){
-			ifs>>nxt>>val;
-			tmp.nxt=nxt;
-			tmp.value=val;
-			node[i].child.push_back(tmp);
-			tmp.nxt=i;
-			node[nxt].father.push_back(tmp);		
-		}
-	}
-	ifs>>s>>e>>o;
-	for(int i=0;i<o;i++){
-		ifs>>of;
-//		node[of].on_fire_t=0;
-		bfs_fire_spread(0,of);
-	}
-}
+node_struct node[200];
+int n,s,e,yourdad[2000]={};
+void bfs_fire_out(int,int);
 string int2str(int num){
 	string a;
 	while(num){
 		a+=(char)((num%10)+'0');
-		a+=' ';
 		num/=10;
 	}
 	string reversed(a);
-//	reverse(reversed.begin(),reversed.end());	
+	reverse(reversed.begin(),reversed.end());	
 	return reversed;
 }
-int tot_path=0;
+void init();
+void input_data(){
+	int x,a,b,o,f_in;
+	pair_s tmp;
+	ifstream f_data,f_xy;
+	string read_in;
+	f_data.open("input.txt");
+	f_data>>n;
+	init();
+	for(int i=1;i<=n;i++){//逐一輸入每個node的資訊
+		f_data>>x;
+		for(int v=0;v<x;v++){
+			f_data>>a>>b;
+			tmp.nxt=a,tmp.value=b;
+			node[i].child.push_back(tmp);
+			tmp.nxt=i;
+			node[a].father.push_back(tmp);
+		}
+	}
+	f_data>>s>>e>>o;
+	for(int v=0;v<o;v++){
+		f_data>>f_in;
+		// cout<<f_in;
+		node[f_in].on_fire_t=0;
+		 bfs_fire_out(f_in,0);
+	}
+}
+void bfs_fire_out(int pos,int t){
+	t*=4;
+	if(t<=node[pos].on_fire_t){
+		node[pos].on_fire_t=t;
+		for(int i=0;i<node[pos].child.size();i++){
+			bfs_fire_out(node[pos].child[i].nxt,t+node[pos].child[i].value);
+		}
+		for(int i=0;i<node[pos].father.size();i++){
+			bfs_fire_out(node[pos].father[i].nxt,t+node[pos].father[i].value);
+		}
+	}
+	else{
+		return ;
+	}
+}
+void bellmanfort(int u){
+	int start=u;
+	node[start].now_value=0;
+	bool change=1;
+	for(int i=1;i<=n&&change;i++){
+		change=0; 
+		for(int j=1;j<=n;j++){
+			for(int g=0;g<node[j].child.size();g++){
+				if(node[j].now_value<inf){
+					if(node[node[j].child[g].nxt].now_value>node[j].now_value+node[j].child[g].value && node[j].now_value+node[j].child[g].value<node[node[j].child[g].nxt].on_fire_t){
+			            node[node[j].child[g].nxt].now_value = node[j].now_value+node[j].child[g].value;
+			            node[node[j].child[g].nxt].from = j;
+			            change = true;
+			          }	
+				}
+			}
+		}
+	} 
+}
+int tot_path;
 string complete_path (int v, int j)
 {
   string p;
@@ -120,26 +108,31 @@ string complete_path (int v, int j)
 		break;
 	  }
   }
-  return p+complete_path(v,node[j].from);
+  return p+" "+complete_path(v,node[j].from);
 }
-int main()
-{
-  input();
-  for (int i=1; i<=n; i++)
-  {
-  	cout<<node[i].on_fire_t<<" ";
-  	node[i].now_min=inf;
-    node[i].from=-1;
-  }
-  bellmanford(s);
-  string result;
-  result+=complete_path(s,e);
-  ofstream osf;
-  osf.open("output.txt");
-  reverse(result.begin(),result.end());
-  result+=' ';
-  result+=int2str(e);
-  cout<<result;
-  osf<<result<<" "<<tot_path;
-  return 0;
+void init(){
+	for(int i=1;i<=n;i++){
+		node[i].from=0;
+		node[i].now_value=inf;
+		node[i].on_fire_t=inf;
+		node[i].was_father=0;
+	}
+}
+int main(){
+input_data();
+for(int i=1;i<=n;i++){
+	cout<<node[i].on_fire_t<<" ";
+}
+bellmanfort(1);
+// cout<<"!";
+string result=int2str(e)+' ';
+for(int i=1;i<=n;i++){
+	cout<<node[i].from<<"! ";
+}
+result+=complete_path(s,e);
+//result+=int2str(e);
+ofstream opt("output.txt");
+cout<<"\n"<<result<<tot_path;
+opt<<result<<" "<<tot_path;
+return 0;
 }
