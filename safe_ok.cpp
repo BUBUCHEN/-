ac10323dbb300de5ab001310677f01ecd67b95bc
BUBUCHEN <75531008+BUBUCHEN@users.noreply.github.com>
2022-03-15 12:52:20 +0800

    Update safe_ok.cpp

--- safe_ok.cpp
+++ safe_ok.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include<vector>
 #include<fstream>
-#define inf 2147483647;
+#define inf 2147483647
 using namespace std;
 struct pair_s{//自製pair
 		int nxt;//下一個節點
@@ -21,7 +21,7 @@ struct node_struct{
 	vector<pair_s> father;
 };
 node_struct node[200000];
-int n,s,e;
+int n,s,e,yourdad[200000]={};
 void bfs_fire_out(int,int);
 string int2str(int num){
 	string a;
@@ -75,24 +75,45 @@ void bfs_fire_out(int pos,int t){
 		return ;
 	}
 }
-int n_min=-1*inf;
-string ph;
-void dfs(int pos,int tot,string recoder,int actual_val){
-	if(node[pos].on_fire_t==0||actual_val>node[pos].on_fire_t){
-		return ;
-	}
-	if(pos==e){
-//	cout<<recoder<<"\n";	
-		if(n_min<tot){
-//			cout<<n_min;
-			n_min=tot;
-			ph=recoder;	
+void dijkstra(int u){
+	int start=u,min=inf;
+	node[start].now_value=0;
+	for(int i=0;i<n;i++){
+		min=inf;
+		for(int j=1;j<=n;j++){
+			if (min> node[j].now_value and node[j].was_father==0)
+			{
+				min = node[j].now_value;
+				start = j;
+			}
 		}
-	return ;
+		node[start].was_father=1;
+		for (int k=0; k<node[start].child.size(); k++)
+		{
+				if (node[node[start].child[k].nxt].now_value >node[start].now_value + node[start].child[k].value && node[start].now_value+node[start].child[k].value<node[node[start].child[k].nxt].on_fire_t)
+				{
+					node[node[start].child[k].nxt].now_value = node[start].now_value + node[start].child[k].value;
+					node[node[start].child[k].nxt].from = start;
+				}
+		}
 	}
-	for(int i=0;i<node[pos].child.size();i++){
-		dfs(node[pos].child[i].nxt,tot+node[node[pos].child[i].nxt].on_fire_t,recoder+" "+int2str(node[pos].child[i].nxt),actual_val+node[pos].child[i].value);
-	}
+}
+int tot_path;
+string complete_path (int v, int j)
+{
+  string p;
+  if(v==j)
+  {
+    return "";
+  }
+  p = int2str(node[j].from);
+  for(int i=0;i<node[node[j].from].child.size();i++){
+  	if(node[node[j].from].child[i].nxt==j){
+  		tot_path+=node[node[j].from].child[i].value;
+		break;
+	  }
+  }
+  return p+" "+complete_path(v,node[j].from);
 }
 void init(){
 	for(int i=1;i<=n;i++){
@@ -114,13 +135,22 @@ input_data();
 for(int i=1;i<=n;i++){
 	cout<<node[i].on_fire_t<<" ";
 }
-cout<<"\n";
-dfs(s,0,int2str(s),0);
-cout<<ph;
+int n_min=-1*inf;
+for(int i=1;i<=n;i++){
+	if(node[i].on_fire_t>n_min&&node[i].on_fire_t!=inf){
+		n_min=node[i].on_fire_t;
+		e=i;
+	}
+} 
+cout<<e;
+//return 0;
+dijkstra(1);
+string result=int2str(e)+' ';
+result+=complete_path(s,e);
 ofstream opt("output.txt");
 QueryPerformanceCounter(&nEndTime);//停止計時  
 time = (double)(nEndTime.QuadPart - nBeginTime.QuadPart) / (double)nFreq.QuadPart;
-//cout<<result<<tot_path<<" "<<1000*time;
-opt<<ph<<" "<<n_min<<" "<<1000*time;
+cout<<result<<tot_path<<" "<<1000*time;
+opt<<result<<" "<<tot_path<<" "<<1000*time;
 return 0;
 }
